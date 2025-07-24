e6c45e6a2c8ef4ec275003ea8f387f47bb1c5e6e
Yang <yang@YangdeMacBook-Pro-2.local>
2025-07-24 14:05:22 -0400

    implement 0020_ValidParentheses

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 | 0001 | Two Sum | Easy | Array | Hash Table | TypeScript | 2025.07.23 |
 | 0009 | Palindrome Number | Easy | Math |  | TypeScript | 2025.07.23 |
 | 0014 | Longest Common Prefix | Easy | Array | String, Trie | TypeScript | 2025.07.23 |
+| 0020 | Valid Parentheses | Easy | Stack | String | TypeScript | 2025.07.23 |
 | 0027 | Remove Element | Easy | Array | Two Pointers | TypeScript | 2025.07.24 |
 |  |  |  |  |  |  |  |
---
+++ Stack/Easy/0020_ValidParentheses.ts
@@ -0,0 +1,22 @@
+// LeetCode    0020 - Valid Parentheses
+// Topic:      Stack
+// Difficulty: Easy
+
+function isValid(s: string): boolean {
+    let stack: string[] = [];
+    if (s === "") return false;
+    const map: { [key: string]: string } = {
+        ")": "(",
+        "}": "{",
+        "]": "[",
+    }
+
+    for (let char of s) {
+        if (char === "(" || char === "{" || char === "[") {
+            stack.push(char);
+        } else {
+            if (stack.pop() !== map[char]) return false;
+        }
+    }
+    return stack.length === 0;
+};
